26554c91c0f2c8bc1fa866721bd0a7d25e9cbb9b
Usmann Khan <usmann@usmannkhan.com>
2019-11-22 11:43:48 -0800

    Initial commit

---
+++ download_data.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+wget https://sid.erda.dk/public/archives/daaeac0d7ce1152aea9b61d9f1e19370/GTSRB_Final_Training_Images.zip
+unzip GTSRB_Final_Training_Images.zip
+rm GTSRB_Final_Training_Images.zip
