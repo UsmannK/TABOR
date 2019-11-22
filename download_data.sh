91b0adc29f460ae50e7eb2adf3f4f9aba0528c7d
Usmann Khan <usmann@usmannkhan.com>
2019-11-22 14:17:10 -0800

    Pipe zip logs to /dev/null

--- download_data.sh
+++ download_data.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 wget https://sid.erda.dk/public/archives/daaeac0d7ce1152aea9b61d9f1e19370/GTSRB_Final_Training_Images.zip
-unzip GTSRB_Final_Training_Images.zip
+unzip GTSRB_Final_Training_Images.zip > /dev/null
 rm GTSRB_Final_Training_Images.zip
